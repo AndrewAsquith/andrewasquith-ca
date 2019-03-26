666169db76073e7ca84e1cfdec6c16ef2f18ac98
Andrew Asquith <contact@andrewasquith.com>
2019-03-26 14:00:42 -0400

    added absolute url filter

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,7 @@ module.exports = function(config) {
     //filters
     config.addFilter('friendlydate', require('./_11ty/filters/friendly-date.js'));
     config.addFilter('isoDate', require('./_11ty/filters/iso-date.js'));
+    config.addFilter("absoluteUrl", require('./_11ty/filters/absoluteUrl.js'));
 
     //Minify HTML (when ELEVENTY_ENV is production)
     config.addTransform('htmlmin', require('./_11ty/transforms/html-minify.js'));
