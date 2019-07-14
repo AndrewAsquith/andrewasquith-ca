f2ba72515342a18ed4d6ea0ea6e98655df95202d
Andrew Asquith <contact@andrewasquith.com>
2019-07-14 12:37:57 -0400

    moved social icons to inline svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require("gulp");
 require('require-dir')('./_gulp');
 
-//gulp.task('build',  gulp.series('clean',gulp.parallel('generate','styles','scripts','images')));
 gulp.task('build',  gulp.series('clean',gulp.parallel('generate','styles', /* 'scripts',*/ 'images')));
