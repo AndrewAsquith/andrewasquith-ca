c877d218abd28661f7d21402eb46e71d15c45cad
Andrew Asquith <contact@andrewasquith.com>
2019-07-17 21:33:16 -0400

    add gulp run to do build before serve

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,5 @@ const gulp = require("gulp");
 require('require-dir')('./_gulp');
 
 gulp.task('build',  gulp.series('clean',gulp.parallel('generate','styles', /* 'scripts',*/ 'images')));
+gulp.task('buildSync', gulp.series('clean',gulp.parallel('generateSync','styles', /* 'scripts',*/ 'images')));
+gulp.task('run', gulp.series('buildSync', 'serve'));
